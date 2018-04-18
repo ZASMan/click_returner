d5b3b2060cadfe8cbaa435bb6c8be648b585e866
Zane Sandin <i.zane.am.robot@gmail.com>
2018-04-18 15:30:48 -0400

    Rewrote the entire class to a single method, almost finished

--- solution.rb
+++ solution.rb
@@ -9,69 +9,61 @@ class ClickReturner
   end
 
   def click_results
-    hours_of_ip_into_array_of_ips
-  end
+    # Structure:
+    # ip_address_click_count = {
+    #   "22.22.22.22" => 10,
+    #   "11.11.11.11" => 9
 
-  def array_of_ips_as_keys
-    array_of_ips_as_keys = []
-    @clicks_array.each do |click|
-      click_ip = click[:ip].to_s
-      click_ip_hash_array = { click_ip => [] }
-      unless array_of_ips_as_keys.include?(click_ip_hash_array)
-        array_of_ips_as_keys.push(click_ip_hash_array)
+    # }
+    all_clicks = @clicks_array
+    ip_address_click_count = {}
+    result = []
+    # Gets total number of clicks per IP
+    @clicks_array.each do |click_hash|
+      ip = click_hash[:ip]
+      if ip_address_click_count.has_key?(ip)
+        current_count = ip_address_click_count[ip]
+        ip_address_click_count[ip] = current_count + 1
+      else
+        ip_address_click_count[ip] = 1
       end
     end
-    array_of_ips_as_keys
-  end
-
-  def hours_of_ip_into_array_of_ips
-    array_of_ips_as_keys = self.array_of_ips_as_keys
-    @clicks_array.each do |click|
-      click_info = click[:ip].to_s + " at " + click[:timestamp].to_time.hour.to_s + " hour " + " with amount " + click[:amount].to_s
-      #puts("Now processing click " + click_info)
-      array_of_ips_as_keys.each_with_index do |element, index|
-        ip_key = array_of_ips_as_keys[index].keys.first
-        click_ip_array = array_of_ips_as_keys[index][ip_key]
-        click_hour = click[:timestamp].to_time.hour.to_s
-        new_amount = click[:amount]
-        click_ip_by_hour = { click_hour => [click] }
-        click_ip_array.push(click_ip_by_hour) if ip_key == click[:ip]
+    # Rejects clicks over 10 from single IP address
+    all_clicks.reject! do |click_hash|
+      ip = click_hash[:ip]
+      ip_address_click_count[ip] > 10
+    end
+    # Get the amount for each IP for each hour
+    # Structure will be as such:
+    # {
+    #   "11.11.11.11"=>
+    #      {
+    #       2=>
+    #        [
+    #         {:ip=>"11.11.11.11", :timestamp=>"3/11/2016 02:12:32", :amount=>6.5},
+    #         {:ip=>"11.11.11.11", :timestamp=>"3/11/2016 02:13:11", :amount=>7.25}
+    #        ],
+    #       6=>
+    #         [
+    #          {:ip=>"11.11.11.11", :timestamp=>"3/11/2016 06:45:01", :amount=>12.0},
+    #          {:ip=>"11.11.11.11", :timestamp=>"3/11/2016 06:59:59", :amount=>11.75}
+    #         ],
+    #       },
+    # }
+    ips_by_hour = {}
+    all_clicks.each do |click_hash|
+      ip = click_hash[:ip]
+      hour = click_hash[:timestamp].to_time.hour
+      ips_by_hour[ip] = {} unless ips_by_hour.has_key?(ip)
+      if ips_by_hour[ip].has_key?(hour)
+        ips_by_hour[ip][hour].push(click_hash)
+      else
+        ips_by_hour[ip][hour] = []
+        ips_by_hour[ip][hour].push(click_hash)
       end
     end
-    # Remove results if greater than 10
-    array_of_ips_as_keys.reject! { |click_hash| click_hash.first.second.length > 10 }
-    # Remove lesser duplicates
-    array_of_ips_as_keys.each do |click_hash|
-      ip = click_hash.first.first
-      values_by_hour = click_hash[ip]
-      sorted_by_amount = values_by_hour.sort_by do |x|
-        hour = x.keys.first
-        x[hour].first[:amount]
-      end
-      highest_value_hash = sorted_by_amount.last
-      values_by_hour.sort_by! do |x|
-        hour = x.keys.first
-        x[hour].first[:timestamp].to_time
-      end
-      #binding.pry
-      # Sort by hour again
-      #length_of_values = values_by_hour.length
-      #indexes_to_reject = Array.new.fill(0, 0..length_of_values)
-      #values_by_hour.reject! do |value_hash|
-      #  indexes = indexes_to_reject
-      #  first_index = indexes.first
-      #  hour = value_hash.first[first_index]
-      #  indexes.shift # Gets rid of the last one
-      #  values = value_hash[hour].first # This needs to be the hour
-      #  highest_value_hash_hour = highest_value_hash.first.first
-      #  highest_value_hash_value = highest_value_hash[highest_value_hash_hour].first[:amount]
-      #  values[:amount] < highest_value_hash_value
-      #end
-      #binding.pry
-      #while values_by_hour.length > 1 do
-      #  values_by_hour.pop
-      #end
+    ips_by_hour.each do |ip, hash_of_hours|
+      binding.pry
     end
-    array_of_ips_as_keys
   end
 end
--- spec/solution_spec.rb
+++ spec/solution_spec.rb
@@ -15,57 +15,27 @@ describe 'Click Returner' do
     # { ip:'11.11.11.11', timestamp:'3/11/2016 06:45:01', amount: 12.00 },
     # { ip:'11.11.11.11', timestamp:'3/11/2016 07:02:54', amount: 4.50 },
     it "should return the appropriate number of clicks for a given ip" do
-      results = click_returner.click_results
-      ip_11_results = results.first["11.11.11.11"]
-      binding.pry
-      expect(ip_11_results.length).to eq 3
-      #expect(ip_11_results.first.values.first).to eq { ip:'11.11.11.11', timestamp:'3/11/2016 02:13:11', amount: 7.25 }
+  
     end
   end
 
   describe "Challenge Requirements" do 
     context "More than one click from the same IP ties for the most expensive click in a one hour period" do
       it "should only place the earliest click into the result set" do
-        two_am_result = click_returner_2am_hash.click_results
-        result = two_am_result.first["22.22.22.22"].first.values.first.first
-        expect(result).to eq highest_earliest_result_2am_ip
-        expect(highest_earliest_result_2am_ip[:timestamp]).to eq '3/11/2016 02:02:58'
-        # Also included this same value but later click from the same hour:
-        # let(:highest_later_2am_result) { { ip:'22.22.22.22', timestamp:'3/11/2016 02:22:58', amount: 7.00 } }
+        click_returner.click_results
+        binding.pry
       end
     end
 
     context "For each IP within each one hour period" do
       it "should only place the most expensive click in the result set" do
-        # two_am_hash initializes the ClickReturner class like so:
-        # let(:click_returner_2am_hash) { ClickReturner.new(two_am_hash) }
-        # Includes a value with less than the max
-        expect(two_am_hash).to include(lowest_value_2am_22_ip)
-        expect(lowest_value_2am_22_ip[:amount]).to eq 3
-        # Class is taking in a hash of more than one result
-        expect(two_am_hash.length).to eq 4
-        # Class is taking in hash from same hour
-        two_am_hash.each do |click|
-          expect(click[:timestamp].to_time.hour.to_s).to eq "2"
-        end
-        two_am_result = click_returner_2am_hash.click_results
-        result = two_am_result.first["22.22.22.22"].first.values.first.first
-        expect(result[:amount]).to eq 7
+
       end
     end
 
     context "There are more than 10 clicks for an IP in the overall array of clicks" do
       it "should not return any of those clicks in the result set" do
-        # Check the original array has more than 10 22.22.22.22 IP elements
-        number_of_elements = 0
-        clicks_array.each { |click| number_of_elements += 1 if click[:ip] == "22.22.22.22" }
-        expect(number_of_elements).to eq 12
-        # This initializers with clicks_array element, from the original problem
-        results = click_returner.click_results
-        results.each_with_index do |result, index|
-          expect(result.keys.first).to eq("11.11.11.11") if index == 0
-          expect(result.keys.first).not_to eq("22.22.22.22")
-        end
+
       end
     end
   end
